cb1794fcd224feae008d2799e41e22413038b816
danang <danangindrak@gmail.com>
2018-01-22 15:59:07 +0700

    Initial commit

---
+++ app/src/main/java/com/ebc/locator/MainActivity.java
@@ -0,0 +1,70 @@
+package com.ebc.locator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.content.Intent;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
+public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private LocationManager mLocationManager;
+    private LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.v(TAG, "Location update: " + location);
+            Log.d(TAG, "Latitude :" + location.getLatitude() + ", Longitude :" + location.getLongitude() + ", provider :"+location.getProvider()+" was read");
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startLocationRequest();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopLocationRequest();
+    }
+
+    private void startLocationRequest() {
+        this.startService(new Intent(this, GpsService.class));
+
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        // We need permission to get location updates
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // A problem occurred auto-granting the permission
+            Log.d(TAG, "No permission");
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                0, 0, mLocationListener);
+    }
+
+    private void stopLocationRequest() {
+        this.stopService(new Intent(this, GpsService.class));
+        mLocationManager.removeUpdates(mLocationListener);
+    }
+}
